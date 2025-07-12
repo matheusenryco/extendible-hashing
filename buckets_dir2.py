8d2fc5f5f0f7895d578a6b547b69e60602dae334
matheus enryco <matheusenryco05@gmail.com>
2025-07-12 18:09:06 -0300

    docs: Improve comments

--- buckets_dir2.py
+++ buckets_dir2.py
@@ -1,16 +1,16 @@
-## pseudocodigo slide 05 ##
+## pseudocode slide 05 ##
 
 from dataclasses import dataclass
 
-TAM_MAX_BUCKET = 2  # colocando o tamanho máximo do bucket como 2
+MAX_BUCKET_SIZE = 2  # setting the maximum bucket size as 2
 
 class Bucket:
-    def __init__(self, profundidade):
-        self.prof: int = profundidade #armazena a profundidade do bucket
-        self.cont = 0
-        self.chaves = []
+    def __init__(self, depth):
+        self.depth: int = depth # stores the bucket depth
+        self.count = 0 # int that stores the number of keys in the bucket
+        self.keys = []
 
-class Diretorio:
+class Directory:
     def __init__(self):
-        self.refs: list = [] #lista de referencia(RRN) aos buckets
-        self.prof_dir: int = 0
+        self.refs: list = [] # list of references (RRN) to buckets
+        self.dir_depth: int = 0 # int that stores the directory depth
