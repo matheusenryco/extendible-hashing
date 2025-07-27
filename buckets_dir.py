0df57f4e917933bdcfaedaf2b931ba6ce034631c
matheus enryco <matheusenryco05@gmail.com>
2025-07-27 00:40:39 -0300

    adding a few comments

---
+++ buckets_dir.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+MAX_BUCKET_SIZE = 2  # setting the maximum bucket size as 2
+
+class Bucket:
+    def __init__(self, depth):
+        self.depth: int = depth # stores the bucket depth
+        self.count = 0 # int that stores the number of keys in the bucket
+        self.keys = []
+
+class Directory:
+    def __init__(self):
+        self.refs: list = [] # list of references (RRN) to buckets
+        self.dir_depth: int = 0 # int that stores the directory depth
