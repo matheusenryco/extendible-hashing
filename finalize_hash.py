94c15124e97f962cbf8e8e951c9fd47aa0bd13ec
matheus enryco <matheusenryco05@gmail.com>
2025-07-27 14:33:32 -0300

    removing a few comments

--- finalize_hash.py
+++ finalize_hash.py
@@ -2,8 +2,7 @@ import struct
 
 def finalize_hash(self, directory):
     with open(self.dir_file, "wb") as f:
-        # Write the directory object to the directory file
-        f.write(struct.pack("<i", directory.dir_depth))  # Writes dir_depth
-        f.write(struct.pack("<i", len(directory.refs)))  # Writes number of references
-        for ref in directory.refs:  # Writes each reference (RRN)
+        f.write(struct.pack("<i", directory.dir_depth))
+        f.write(struct.pack("<i", len(directory.refs)))
+        for ref in directory.refs:
             f.write(struct.pack("<i", ref))
